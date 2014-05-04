c2f73537e9badda792f93620dd1b34ddc9801882
Ralph Khattar <ralph.khattar@gmail.com>
2014-05-04 00:58:19 -0400

    Cleaned up notices

--- lib/CompiledStore.php
+++ lib/CompiledStore.php
@@ -3,7 +3,7 @@
 /**
  * This file is a part of Codify PHP.
  *
- * (c) 2014 Ralph khattar
+ * (c) 2014 Ralph Khattar
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- lib/Stores/Memory.php
+++ lib/Stores/Memory.php
@@ -11,6 +11,15 @@
 
 namespace Codify\Stores;
 
+/**
+ * This file is a part of Codify PHP.
+ *
+ * (c) 2014 Ralph Khattar
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Codify\Store;
 
 /**
